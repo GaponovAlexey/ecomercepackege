56566b0b8779fde10758af2e07e9593eaadd2d20
kekelberi <alexei.gaponov@gmail.com>
2022-06-17 17:38:14 +0300

    save

---
+++ components/layout/layout.tsx
@@ -0,0 +1,20 @@
+const Layout = ({ children }: any) => {
+  return (
+    <div>
+      <header>
+        <div className='grid grid-cols-2 px-2 bg-slate-400'>
+          <h1>ICO</h1>
+          <div className='flex justify-between  '>
+            <button>test1</button>
+            <button>test2</button>
+            <button>test3</button>
+          </div>
+        </div>
+      </header>
+      <main className='min-h-screen'>{children}</main>
+      <footer className='p-2 text-center bg-slate-400'>footer</footer>
+    </div>
+  )
+}
+
+export default Layout
