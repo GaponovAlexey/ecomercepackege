25a0c50d2c38d0244415d2e688e12f778298c236
kekelberi <alexei.gaponov@gmail.com>
2022-06-17 20:03:39 +0300

    save

--- src/components/Acordion.tsx
+++ src/components/Acordion.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { styled, keyframes } from '@stitches/react'
-import { violet, blackA, mauve } from '@radix-ui/colors'
+import { violet, blackA, } from '@radix-ui/colors'
 import { ChevronDownIcon } from '@radix-ui/react-icons'
 import * as AccordionPrimitive from '@radix-ui/react-accordion'
 
@@ -16,9 +16,10 @@ const slideUp = keyframes({
 
 const StyledAccordion = styled(AccordionPrimitive.Root, {
   borderRadius: 6,
+  height: '100%',
   width: '100%',
-  backgroundColor: mauve.mauve6,
-  boxShadow: `0 2px 10px ${blackA.blackA4}`,
+  backgroundColor: blackA.blackA1,
+  boxShadow: `0 2px 10px ${blackA.blackA1}`,
 })
 
 const StyledItem = styled(AccordionPrimitive.Item, {
@@ -39,7 +40,7 @@ const StyledItem = styled(AccordionPrimitive.Item, {
   '&:focus-within': {
     position: 'relative',
     zIndex: 1,
-    boxShadow: `0 0 0 2px ${mauve.mauve12}`,
+    boxShadow: `0 0 0 2px ${blackA.blackA1}`,
   },
 })
 
@@ -61,17 +62,16 @@ const StyledTrigger = styled(AccordionPrimitive.Trigger, {
   fontSize: 21,
   lineHeight: 1,
   color: 'green',
-  boxShadow: `0 1px 0 ${mauve.mauve6}`,
-  '&[data-state="closed"]': { backgroundColor: 'white' },
-  '&[data-state="open"]': { backgroundColor: 'white' },
-  '&:hover': { backgroundColor: mauve.mauve2 },
+  boxShadow: `0 1px 0 ${blackA.blackA4}`,
+  '&[data-state="closed"]': { backgroundColor: 'black' },
+  '&[data-state="open"]': { backgroundColor: 'black' },
+  '&:hover': { backgroundColor: blackA.blackA1 },
 })
 
 const StyledContent = styled(AccordionPrimitive.Content, {
-  overflow: 'hidden',
   fontSize: 15,
-  color: mauve.mauve11,
-  backgroundColor: mauve.mauve2,
+  color: 'silver',
+  backgroundColor: blackA.blackA3,
 
   '&[data-state="open"]': {
     animation: `${slideDown} 300ms cubic-bezier(0.87, 0, 0.13, 1) forwards`,
@@ -118,7 +118,11 @@ export const AccordionDemo = () => (
     <AccordionItem value='item-1'>
       <AccordionTrigger>what can i do?</AccordionTrigger>
       <AccordionContent>
-        Yes. It adheres to the WAI-ARIA design pattern.
+        Yes. It adheres to the WAI-ARIA design pattern.Yes. It adheres to the
+        WAI-ARIA design pattern.Yes. It adheres to the WAI-ARIA design
+        pattern.Yes. It adheres to the WAI-ARIA design pattern.Yes. It adheres
+        to the WAI-ARIA design pattern.Yes. It adheres to the WAI-ARIA design
+        pattern.
       </AccordionContent>
     </AccordionItem>
 
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,16 @@
 import * as Accordion from '@radix-ui/react-accordion'
 import AccordionDemo from './Acordion'
+import DropdownMenuItemComponent from './drop-down'
 const Mein = () => {
   return (
-    <div>
+    <div className='relative'>
       <div className='text-2xl text-center'>Main</div>
-      <section>
+      <section className='mt-4' >
+        <div className='left absolute right-0 top-0 '>
+          <DropdownMenuItemComponent />
+        </div>
         <div>
-        <AccordionDemo />
+          <AccordionDemo />
         </div>
       </section>
     </div>
