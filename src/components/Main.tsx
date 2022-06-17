c9b522082c88d7520b82fbf137e018bb523d7d15
kekelberi <alexei.gaponov@gmail.com>
2022-06-17 20:25:43 +0300

    save

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,16 +1,21 @@
 import * as Accordion from '@radix-ui/react-accordion'
-import AccordionDemo from './Acordion'
-import DropdownMenuItemComponent from './drop-down'
+import AccordionDemo from './radix/Acordion'
+import DropdownMenuItemComponent from './radix/drop-down'
 const Mein = () => {
   return (
     <div className='relative'>
       <div className='text-2xl text-center'>Main</div>
-      <section className='mt-4' >
+      <section className='mt-4'>
+        {/* // radix-ui */}
         <div className='left absolute right-0 top-0 '>
           <DropdownMenuItemComponent />
         </div>
         <div>
           <AccordionDemo />
+        </div>
+        {/* // spring */}
+        <div>
+          <h2 className='text-2xl text-center mt-2'>spring</h2>
         </div>
       </section>
     </div>
