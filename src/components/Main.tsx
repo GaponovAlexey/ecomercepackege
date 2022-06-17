f9c545abd3505786f63268cb6053e6eb12b7ee89
kekelberi <alexei.gaponov@gmail.com>
2022-06-17 18:52:33 +0300

    save

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,12 @@
 import * as Accordion from '@radix-ui/react-accordion'
+import AccordionDemo from './Acordion'
 const Mein = () => {
   return (
     <div>
       <div className='text-2xl text-center'>Main</div>
       <section>
         <div>
-          <Accordion.Root type='single' defaultValue='item-2'>
-            <Accordion.Item value='item-1'>…</Accordion.Item>
-            <Accordion.Item value='item-2'>…</Accordion.Item>
-          </Accordion.Root>
+        <AccordionDemo />
         </div>
       </section>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
-import Mein from '../components/Mein'
+import Mein from '../components/Main'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
