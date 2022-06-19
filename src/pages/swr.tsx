b57c8357e413ac9a17ce4130cf933987fcd538dc
Kekelberi <alexei.gaponov@gmail.com>
2022-06-19 15:38:51 +0300

    save

--- src/pages/swr.tsx
+++ src/pages/swr.tsx
@@ -1,9 +1,20 @@
+import Head from 'next/head'
 import useSWR from 'swr'
 
+export function useUser() {
+  return useSWR('/api/hello')
+}
+
 const SWR = () => {
-  const { data, error } = useSWR('/api/hello')
-  return <div>{data && data.name}</div>
+  const { data } = useUser()
+  return (
+    <>
+      <Head>
+        <title>SWR</title>
+      </Head>
+      <div>{data && data.name}</div>
+    </>
+  )
 }
 
 export default SWR
-
