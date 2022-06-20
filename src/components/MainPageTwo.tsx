7259820d3e612809ea87206a7fef9d4f410595e8
kekelberi <alexei.gaponov@gmail.com>
2022-06-20 19:48:31 +0300

    save

--- src/components/MainPageTwo.tsx
+++ src/components/MainPageTwo.tsx
@@ -1,11 +1,17 @@
 import { useTheme } from 'next-themes'
 import Marquee from 'react-fast-marquee'
 import useMeasure from 'react-use-measure'
+import HoverEfect from './CssCard/HoverEfect'
+import PhotoGaleray from './CssCard/PhotoGaleray'
+import { Example } from './Hooks'
 const MainPageTwo = () => {
   const { theme, setTheme } = useTheme()
   const [ref, bounds] = useMeasure()
   return (
     <div>
+      <button onClick={() => setTheme('light')}>Light Mode</button>
+      <br />
+      <button onClick={() => setTheme('dark')}>Dark Mode</button>
       <Marquee direction={'right'} gradient={false} loop={1}>
         <div>
           <img src='/img/g1.jpg' alt='da' width='300px' />
@@ -26,6 +32,17 @@ const MainPageTwo = () => {
           <br />
           right:{bounds.right}
         </div>
+      </div>
+      {/* image button */}
+      <div>
+        <button className='oneButton'>send</button>
+      </div>
+      <div>
+        <HoverEfect />
+      </div>
+      &#128576; &#128030;
+      <div>
+        <Example />
       </div>
     </div>
   )
